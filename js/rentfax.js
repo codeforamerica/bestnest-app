87cf7fb6336c91ea11b4cef4359580b518c734c7
Jeremia Kimelman <jbkimelman@gmail.com>
2014-05-09 00:33:32 -0700

    working through d3 tutorials to get the chart to be responsive and look like the mock

---
+++ js/rentfax.js
@@ -0,0 +1,122 @@
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 480 - margin.left - margin.right,
+    height = 250 - margin.top - margin.bottom;
+
+var x = d3.scale.linear()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var svg = d3.select(".energy-chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("data/energy.tsv", type, function(error, data) {
+  x.domain(["J", "f", "m", "a", "j", "j", "a", "s", "o", "n", "d"]);
+  y.domain([0, d3.max(data, function(d) { return d.amount; })]);
+
+  barWidth = width / data.length;
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d, i) { return i * barWidth; })
+      .attr("width", barWidth - 10)
+      .attr("y", function(d) { return y(d.amount); })
+      .attr("height", function(d) { return height - y(d.amount); });
+
+});
+
+function type(d) {
+  d.amount = +d.amount;
+  return d;
+}
+
+/*
+
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 500 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var chart = d3.select(".energy-chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("data/energy.tsv", type, function(error, data) {
+  x.domain(data.map(function(d) { return d.month; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  var barWidth = width / data.length;
+
+  chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  chart.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d, i) { return i * barWidth + (barWidth/4); })
+      .attr("width", x.rangeBand() )
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); });
+
+});
+
+function type(d) {
+  d.value = +d.value; // coerce to number
+  return d;
+}
+
+
+/*
+var ctx = document.getElementById("energy-cost-by-month").getContext("2d");
+
+var data = {
+	labels : ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+	datasets : [
+		{
+			fillColor: "rgb(74, 179, 236)",
+			strokeColor: "rgb(74, 179, 236)",
+			data: [65,59,90,81,56,55,40]
+		}
+	]
+};
+
+var options = {
+  scaleShowGridLines: false,
+  scaleGridLineWidth: 0,
+  scaleLineWidth: 0,
+  scaleShowLabels: false
+};
+
+var chart = new Chart(ctx).Bar(data, options);
+
+*/
