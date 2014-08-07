e2f7544132d8a41f610701630ec78fbffa4bf8b4
Jeremia Kimelman <jbkimelman@gmail.com>
2014-08-07 11:23:43 -0700

    views for leaving review

---
+++ views/leaveReview.js
@@ -0,0 +1,26 @@
+var AmpersandView = require('ampersand-view')
+var fs = require('fs')
+var bliss = new (require('bliss'))
+var $ = require('jquery')
+
+var api = require('../api')
+
+var template = bliss.compile(fs.readFileSync(__dirname +'/../templates/leaveReview.html','utf8'))
+
+var LeaveReviewView = AmpersandView.extend({
+  initialize: function () {
+    this.render = this.render.bind(this)
+  },
+  render: function (landlordId) {
+    var view = this
+    view.el = view.el || document.createElement('div')
+    return api.getLandlord(landlordId)
+      .then(function (model) {
+        console.log('model', model)
+        var html = template(model)
+        $(view.el).html(html)
+      })
+  }
+})
+
+module.exports = LeaveReviewView
