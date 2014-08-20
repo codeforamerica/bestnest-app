9117ed717b25c3a8eb3fd58de8556c0f304fffec
Jeremia Kimelman <jbkimelman@gmail.com>
2014-08-20 16:05:34 -0700

    have configuration for nav and content elems

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ var view = require('./view')
 
 $(document).ready(function () {
 
-  view.initialize({rootElement:'#outer'})
+  options = {
+    rootElement:'#outer',
+    navElement:'nav',
+    contentElement: 'content'
+  }
+
+  view.initialize(options)
+
   var router = new Router()
 
-    router.on('route', function (routeName, params) {
-      // assume first route parameter is the homeId
-      var homeId = params ? params[0] : null
-      console.log('r', routeName, arguments)
+  router.on('route', function (routeName, params) {
+    // assume first route parameter is the homeId
+    var homeId = params ? params[0] : null
+    console.log('r', routeName, arguments)
 
-      view.show(routeName, homeId)
-    })
+    view.show(routeName, homeId)
+  })
 
   router.history.start()
 })
